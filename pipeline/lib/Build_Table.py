cb7aef484467b9867dfab0a56fb8b3967727bb6c
Biping Meng <mengbiping@gmail.com>
2015-08-25 15:51:43 +0800

    Allow ref color table to be built in parallel. Allow Color_Classify.py to load pre-built color ref table.

--- pipeline/lib/Build_Table.py
+++ pipeline/lib/Build_Table.py
@@ -1,24 +1,25 @@
 import Image,cv2,os,time,shutil
 import numpy as np
+import argparse
 import pickle
 from colormath.color_objects import LabColor, sRGBColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
+from colormath.color_diff import delta_e_cie1976
 from BackRemove_Mask_Simple import remove_background
 from SkinDetect import skin_detect
+from multiprocessing import Pool
+from functools import partial
 
-color_dir_path = '/home/mengbiping/Documents/color-detection/color-0818/color_32' # the folder of color pictures
-
-# recored the average rgb value for the color pictures as reference
-color_ref_rgb = [] # array of [r,g,b] for the reference colors.
-color_ref_lab = [] # array of lab object for the reference colors.
-color_ref_name = [] # array of color names.
-color_num = 0 # The total number of colors.
-white_index = 0 # The index for white color.
-for root, dirs, files in os.walk(color_dir_path) :
-    for name in files:
+def build_metadata(color_ref_dir):
+    metadata = {}
+    color_ref_rgb = [] # array of [r,g,b] for the reference colors.
+    color_ref_lab = [] # array of lab object for the reference colors.
+    color_ref_name = [] # array of color names.
+    color_num = 0 # The total number of colors.
+    for name in sorted(os.listdir(color_ref_dir)) :
         if name.endswith('.png') or name.endswith('.jpg') :
-            filename = os.path.join(root, name)
+            filename = os.path.join(color_ref_dir, name)
             img = cv2.imread(filename)
             name_length = len(name)
             color_name = name[:name_length-4]
@@ -27,35 +28,52 @@ for root, dirs, files in os.walk(color_dir_path) :
             rgb_obj = sRGBColor(rgb[2]/255.0, rgb[1]/255.0, rgb[0]/255.0)
             color_ref_lab.append(convert_color(rgb_obj, LabColor))
             color_ref_name.append(color_name)
-            if color_name == 'White' :
-                white_index = color_num
             color_num += 1
+    metadata['color_ref_rgb'] = color_ref_rgb
+    metadata['color_ref_lab'] = color_ref_lab
+    metadata['color_ref_name'] = color_ref_name
+    metadata['color_num'] = color_num
+    return metadata
 
-print "Building the color ref matrix."
-rgb_to_color = np.zeros((256,256,256))
-for i in range(256) :
-    for j in range(256) :
-        for k in range(256) :
-            delta = 10000
-            color_index = 0
-            comparison_rgb = sRGBColor(i/255.0, j/255.0, k/255.0)
-            comparison_lab = convert_color(comparison_rgb, LabColor)
-            for l in range (len(color_ref_lab)) :
-                delta_e = delta_e_cie2000(comparison_lab, color_ref_lab[l])
-                if delta_e < delta:
-                    delta = delta_e
-                    color_index = l
-            rgb_to_color[i,j,k] = color_index
-    print "%d finished" % i
+def find_ref_color(color_ref_lab, r, g, b) :
+    comparison_rgb = sRGBColor(r/255.0, g/255.0, b/255.0)
+    comparison_lab = convert_color(comparison_rgb, LabColor)
+    result = map(partial(delta_e_cie2000, comparison_lab), color_ref_lab)
+    delta = min(result)
+    color_index = result.index(delta)
+    return color_index
 
-rgb_table_filehandler = open(b"rgb_to_color_table.obj","wb")
-pickle.dump(rgb_to_color, rgb_table_filehandler)
+def find_all_blue(color_ref_lab, r, g) :
+    return map(partial(find_ref_color, color_ref_lab, r, g), range(256))
 
-metadat_filehandler = open(b"metadata.obj","wb")
-metadata = {}
-metadata['color_ref_rgb'] = color_ref_rgb
-metadata['color_ref_lab'] = color_ref_lab
-metadata['color_ref_name'] = color_ref_name
-metadata['color_num'] = color_num
-metadata['white_index'] = white_index
-pickle.dump(metadata, metadat_filehandler)
+def build_color_table(color_ref_lab) :
+    rgb_to_color = np.zeros((256,256,256))
+    for i in range(256) :
+        pool = Pool(processes=8)
+        rgb_to_color[i] = pool.map(partial(find_all_blue, color_ref_lab, i), range(256))
+        # for j in range(256) :
+        #   for k in range(256) :
+        #        rgb_to_color[i,j,k] = find_ref_color(i, j, k, color_ref_lab)
+        pool.close()
+        pool.join()
+        print "%d finished" % i
+    return rgb_to_color
+
+def main() :
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--metadata", default="metadata.obj")
+    parser.add_argument("--rgb_to_color", default="rgb_to_color_table.obj")
+    parser.add_argument("--color_ref_dir", default="/home/mengbiping/Documents/color-detection/color-0818/color_32")
+    args = parser.parse_args()
+
+    metadat_filehandler = open(b"metadata.obj","wb")
+    metadata = build_metadata(args.color_ref_dir)
+    pickle.dump(metadata, metadat_filehandler)
+
+    print "Building the color ref matrix."
+    rgb_table_filehandler = open(b"rgb_to_color_table.obj","wb")
+    rgb_to_color = build_color_table(metadata['color_ref_lab'])
+    pickle.dump(rgb_to_color, rgb_table_filehandler)
+
+if __name__ == "__main__":
+    main()
--- pipeline/lib/Color_Classify.py
+++ pipeline/lib/Color_Classify.py
@@ -7,98 +7,107 @@ from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
 from BackRemove_Mask_Simple import remove_background
 from SkinDetect import skin_detect
+from Build_Table import build_metadata
+from Build_Table import find_ref_color
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--metadata", default="metadata.obj")
-parser.add_argument("--rgb_to_color", default="rgb_to_color_table.obj")
+parser.add_argument("--metadata", default="")
+parser.add_argument("--rgb_to_color", default="")
 parser.add_argument("--colors_to_decay", default="White,Silver,Black")
 parser.add_argument("--decay_percentage", default="0.7", type=float)
+parser.add_argument("--max_skin_percentage", default="0.6", type=float)
+parser.add_argument("--color_ref_dir", default="/home/mengbiping/Documents/color-detection/color-0818/color_32")
+parser.add_argument("--image_dir", default="bad_cases")
+parser.add_argument("--result_dir", default="result_32_2/")
 args = parser.parse_args()
 
-# calculate the reference common color
-color_dir_path = '/home/mengbiping/Documents/color-detection/color-0818/color_32' # the folder of color pictures
-img_dir_path = '/home/mengbiping/caffe/data/clothes_neck/train' # the folder of images to process
-result_dir_path = 'result_32/' # the folder to save the resulted images
+# Prepare metadata.
+metadata = None
+if len(args.metadata) > 0 :
+  with open(args.metadata, 'r') as m_file:
+      metadata = pickle.load(m_file)
+  m_file.close()
+else :
+  metadata = build_metadata(args.color_ref_dir)
+color_ref_rgb = metadata['color_ref_rgb'] # array of [r,g,b] for the reference colors.
+color_ref_lab = metadata['color_ref_lab'] # array of lab object for the reference colors.
+color_ref_name = metadata['color_ref_name'] # array of color names.
+color_num = metadata['color_num'] # The total number of colors.
 
-# recored the average rgb value for the color pictures as reference
-color_ref_rgb = [] # array of [r,g,b] for the reference colors.
-color_ref_lab = [] # array of lab object for the reference colors.
-color_ref_name = [] # array of color names.
-color_num = 0 # The total number of colors.
-
-with open(args.metadata, 'r') as m_file:
-    metadata = pickle.load(args.metadata)
-    color_ref_rgb = metadata['color_ref_rgb']
-    color_ref_lab = metadata['color_ref_lab']
-    color_ref_name = metadata['color_ref_name']
-    color_num = metadata['color_num']
-m_file.close()
-
+# Prepare decay colors.
 decay_color_index = set() # The index for colors to be decayed, e.g., white and silver.
-decay_color_name = args.colores_to_decay.split(",")
+decay_color_name = args.colors_to_decay.split(",")
 for i in range(len(color_ref_name)) :
-    if color_ref_name[i] in decay_color_name :
-        print "Decaying color: %s" . color_ref_name[i]
-        decay_color_index.add(i)
+  if color_ref_name[i] in decay_color_name :
+    print "Decaying color: %s" % color_ref_name[i]
+    decay_color_index.add(i)
 
+# Prepare the mapping of rgb -> ref_color.
 rgb_to_color = None
-with open(args.rgb_to_color, 'r') as r_file:
-    rgb_to_color = pickle.load(args.rgb_to_color)
-r_file.close()
+if len(args.rgb_to_color) > 0 :
+  with open(args.rgb_to_color, 'r') as r_file:
+      rgb_to_color = pickle.load(r_file)
+  r_file.close()
+else :
+  rgb_to_color = np.full((256,256,256), -1)
 
-# process images
+# Prepare resulting dir.
+if not os.path.exists(args.result_dir)
+  os.mkdir(args.result_dir)
+for name in color_ref_name :
+  path = os.path.join(args.result_dir, name)
+  if not os.path.exists(path)
+    os.mkdir(path)
+
+# Process images
 count = 0
-for root, dirs, files in os.walk(img_dir_path):
-    for name in files:
-        if name.endswith('.jpg'):# search for all the jpg formate images for processing
-	    filename = os.path.join(root,name)
-	    img = cv2.imread(filename)
-            print "Processing image No. %d ..." % count
-	    count = count + 1
-	    start_time = time.time()
-	    # generate the foreground
-            fore = remove_background(filename) # background removal
-            fore1 = fore
-	    skinfore = skin_detect(filename) # skin removal
-            fore = cv2.bitwise_and(fore, fore, mask = skinfore) # the foreground mask 
-	    if 1 - sum(map(sum, fore))/1.0/sum(map(sum, fore1)) > 0.58: # for the case where clothes is regared as skin
-                print "too much skin"
-		fore = fore1
+for name in sorted(os.listdir(args.image_dir)) :
+  if not name.endswith('.jpg'):# search for all the jpg formate images for processing
+    continue
+  filename = os.path.join(args.image_dir, name)
+  img = cv2.imread(filename)
+  print "Processing image No. %d ..." % count
+  count = count + 1
+  start_time = time.time()
+  # generate the foreground
+  fore = remove_background(filename) # background removal
+  fore1 = fore
+  skinfore = skin_detect(filename) # skin removal
+  fore = cv2.bitwise_and(fore, fore, mask = skinfore) # the foreground mask
+  # In case of clothes in skin color.
+  if 1 - sum(map(sum, fore))/1.0/sum(map(sum, fore1)) > args.max_skin_percentage:
+    print "Too much skin"
+  fore = fore1
 
-	    # find the nearest reference color for each pixel and count
-            color_count = [0] * color_num
-	    for i in range(len(fore)):
-	        for j in range(len(fore[0])):
-		    if fore[i][j] == 255:
-                        color_index = rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]]
-	                color_count[color_index] += 1
-            
-            for decay_color in decay_color_index
-                color_count[decay_color] *= args.decay_percentage # for the case of blank area
-	    
-	    result_color_index = 0
-            count_ref = 0
-	    result_color_2 = ''
-            count_ref_2 = 0
-            for l in range(len(color_count)):
-	        if color_count[l] > count_ref:
-		    result_color_2 = result_color_index
-            	    count_ref_2 = count_ref
-		    count_ref = color_count[l]
-	            result_color_index = l
-	    #print result_color_index,count_ref,result_color_2,count_ref_2
-	    
-	    # output the original image and that with background and skin removed showed in alpha channel
-            img = Image.open(filename)
-	    filename1 =  result_dir_path + colore_ref_name[result_color_index] + '/' + str(count)+'.jpg'
-            img.save(filename1)
+  # find the nearest reference color for each pixel and count
+  color_count = [0] * color_num
+  for i in range(len(fore)):
+    for j in range(len(fore[0])):
+      if fore[i][j] != 255:
+        continue
+      if rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]] < 0 :
+        rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]] = find_ref_color(img[i][j][2], img[i][j][1], img[i][j][0], color_ref_lab)
 
-	    img = cv2.imread(filename,-1)
-	    filename1 = result_dir_path + colore_ref_name[result_color_index] + '/' + str(count)+'.png'
-	    b = img[:,:,0]
-	    g = img[:,:,1]
-	    r = img[:,:,2]
-            img_merge = cv2.merge((b,g,r,fore))
-            cv2.imwrite(filename1,img_merge)
+      color_index = int(rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]])
+      color_count[color_index] += 1
+      for decay_color in decay_color_index :
+        color_count[decay_color] *= args.decay_percentage # for the case of blank area
 
-	    print("---image %d: %s, %s seconds ---" % (count, colore_ref_name[result_color_index], (time.time() - start_time)))
+  max_color_count = max(color_count)
+  result_color_index = color_count.index(max_color_count)
+  #print result_color_index,count_ref,result_color_2,count_ref_2
+
+  # output the original image and that with background and skin removed showed in alpha channel
+  img = Image.open(filename)
+  filename1 =  args.result_dir + color_ref_name[result_color_index] + '/' + str(count)+'.jpg'
+  img.save(filename1)
+
+  img = cv2.imread(filename,-1)
+  filename1 = args.result_dir + color_ref_name[result_color_index] + '/' + str(count)+'.png'
+  b = img[:,:,0]
+  g = img[:,:,1]
+  r = img[:,:,2]
+  img_merge = cv2.merge((b,g,r,fore))
+  cv2.imwrite(filename1,img_merge)
+
+  print("---image %d: %s, %s seconds ---" % (count, color_ref_name[result_color_index], (time.time() - start_time)))
