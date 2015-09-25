4b422d9f0e0cf4b19921f71eda37f1aa7024f929
guixinlu <guixinlu@pku.edu.cn>
2015-09-25 10:50:03 +0800

    Update color_classifier.py
    fix error for divide zero and reduce IO once

--- pipeline/lib/color_classifier.py
+++ pipeline/lib/color_classifier.py
@@ -16,7 +16,7 @@ class ColorClassifier(object) :
   def __init__(self, metadata_file_path=None, ref_color_table_file_path=None,
           color_ref_dir=None, decay_colors="", decay_percentage=0.7,
           max_skin_percentage=0.6) :
-
+    
     metadata = None
     if metadata_file_path is not None and len(metadata_file_path) > 0 :
       with open(metadata_file_path, 'r') as m_file:
@@ -32,6 +32,7 @@ class ColorClassifier(object) :
       rgb_to_color = None
       with open(ref_color_table_file_path, 'r') as r_file:
         rgb_to_color = pickle.load(r_file)
+        print "load existed color table"
       r_file.close()
       self._color_table_builder.load_color_table(rgb_to_color)
 
@@ -54,15 +55,15 @@ class ColorClassifier(object) :
     start_time = time.time()
     # generate the foreground
     background_removed = remove_background(image_file_path) # background removal
-    skin_removed = skin_detect(image_file_path) # skin removal
+    skin_removed = skin_detect(img) # skin removal
     foreground = cv2.bitwise_and(background_removed, background_removed, mask = skin_removed) # the foreground mask
-    skin_percentage = 1 - sum(map(sum, foreground))/1.0/sum(map(sum, background_removed))
+    skin_percentage = 1 - sum(map(sum, foreground))/1.0/(sum(map(sum, background_removed))+0.1)
     # In case of clothes in skin color.
     # print "%f skin detected." % skin_percentage
     if skin_percentage > self._max_skin_percentage:
       # print "Too much skin"
       foreground = background_removed
-
+  
     # Find the nearest reference color for each pixel and count
     color_histogram = [0] * self._color_num
     image_foreground_pixel = 0
@@ -73,18 +74,18 @@ class ColorClassifier(object) :
         image_foreground_pixel += 1
         if self._color_table_builder.rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]] < 0 :
           self._color_table_builder.reset_color(img[i][j][2], img[i][j][1], img[i][j][0])
-
+  
         color_index = int(self._color_table_builder.rgb_to_color[img[i][j][2], img[i][j][1], img[i][j][0]])
         color_histogram[color_index] += 1
 
     # Decay colors.
     for decay_color in self._decay_color_index :
       color_histogram[decay_color] *= self._decay_percentage
-
+  
     max_color_count = max(color_histogram)
     return (color_histogram.index(max_color_count),
         color_histogram,
-        max_color_count / float(image_foreground_pixel),
+        max_color_count / (float(image_foreground_pixel)+0.1),
         foreground)
 
   def color_index_to_name(self, index) :
@@ -100,9 +101,9 @@ def main() :
   parser.add_argument("--colors_to_decay", default="White,Silver,Black,Chocolate")
   parser.add_argument("--decay_percentage", default="0.7", type=float)
   parser.add_argument("--max_skin_percentage", default="0.8", type=float) # 0.8 for intimates and 0.6 for others.
-  parser.add_argument("--color_ref_dir", default="/home/mengbiping/Documents/color-detection/color-0818/color_32")
-  parser.add_argument("--image_dir", default="/home/mengbiping/caffe/data/clothes_neck/train")
-  parser.add_argument("--result_dir", default="result_32_2/")
+  parser.add_argument("--color_ref_dir", default="color_32")
+  parser.add_argument("--image_dir", default="../../../crawler")
+  parser.add_argument("--result_dir", default="../../result_amazon_190k/")
   args = parser.parse_args()
   classifier = ColorClassifier(args.metadata, args.rgb_to_color,
           args.color_ref_dir, args.colors_to_decay, args.decay_percentage,
@@ -118,6 +119,9 @@ def main() :
   for name in sorted(os.listdir(args.image_dir)) :
     if not name.endswith('.jpg'):# search for all the jpg formate images for processing
       continue
+    if name < '137599.jpg':# search for all the jpg formate images for processing
+      count += 1
+      continue
     print "Processing image No. %d: %s" % (count, name)
     filename = os.path.join(args.image_dir, name)
     start_time = time.time()
@@ -131,7 +135,7 @@ def main() :
       current_index = top_three_color[i]
       print "Top %d color: %s %f" % (i + 1,
               classifier.color_index_to_name(current_index),
-              color_histogram[current_index] / totol_foreground_pixels)
+              color_histogram[current_index] / (totol_foreground_pixels+0.1))
 
 
     # output the original image and that with background and skin removed showed in alpha channel
@@ -139,13 +143,13 @@ def main() :
     filename1 =  args.result_dir + result_color_name + '/' + str(count)+'.jpg'
     img.save(filename1)
 
-    img = cv2.imread(filename,-1)
-    filename1 = args.result_dir + result_color_name + '/' + str(count)+'.png'
-    b = img[:,:,0]
-    g = img[:,:,1]
-    r = img[:,:,2]
-    img_merge = cv2.merge((b, g, r, foreground_mask))
-    cv2.imwrite(filename1, img_merge)
+    #img = cv2.imread(filename,-1)
+    #filename1 = args.result_dir + result_color_name + '/' + str(count)+'.png'
+    #b = img[:,:,0]
+    #g = img[:,:,1]
+    #r = img[:,:,2]
+    #img_merge = cv2.merge((b, g, r, foreground_mask))
+    #cv2.imwrite(filename1, img_merge)
 
     print("---image %d: %s, %s seconds ---" % (count, result_color_name, (time.time() - start_time)))
     count = count + 1
