7687a1f95777162d9eb5ab62a348c67f1a18842c
Wenwei Yu <yuww82@gmail.com>
2015-10-24 15:56:28 -0700

    Update 6.detect.py and add a flag to specify the path of model

--- pipeline/6.detect.py
+++ pipeline/6.detect.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python
-import numpy as np
-import matplotlib.pyplot as plt
-import pprint
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import pprint
 import os
 import re
-
-os.chdir(os.getenv("HOME") + '/caffe')
 import sys
-sys.path.insert(0, 'python')
+
+sys.path.insert(0, os.path.join(os.getenv('HOME'), 'caffe', 'python'))
 import caffe
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_iter_in_k", default="40")
-parser.add_argument("--data_name", default="clothes_neck")
+parser.add_argument("--data_name", default="clothes")
 parser.add_argument("--net_name", default="googlenet")
 parser.add_argument("--snapshot", default="quick_snapshots")
 parser.add_argument("--mode", default="gpu")
 parser.add_argument("--min_confidence", default="0.7", type=float)
+parser.add_argument("--model_path_prefix", default="/mnt/data",
+        help="The path prefix to save the model files.")
 args = parser.parse_args()
 
 # Set the right path to your model definition file, pretrained model weights,
 # and the image you would like to classify.
 model_name = "%s_%s" % (args.data_name, args.net_name)
-model_file = "models/%s/%s/%s_iter_%s000.caffemodel" %(model_name, args.snapshot, model_name, args.model_iter_in_k)
-deploy_file = "models/%s/deploy.prototxt" % model_name
+_model_filename = '{}_iter_{}.caffemodel'.format(model_name, args.model_iter_in_k)
+model_file = os.path.join(args.model_path_prefix, 'models', model_name,
+        args.snapshot, _model_filename)
+deploy_file = os.path.join('models', model_name, 'deploy.prototxt')
 
-mean_file = 'data/%s/%s_mean.npy' % (args.data_name, args.data_name)
-image_dir = 'data/%s/test/' % args.data_name
-#IMAGE_FILES = [ IMAGE_PATH + 'v-test1.jpg', IMAGE_PATH + 'v-test2.jpg', IMAGE_PATH + 'v-test3.jpg',
-#                IMAGE_PATH + 'round-test1.jpg', IMAGE_PATH + 'round-test2.jpg', IMAGE_PATH + 'round-test3.jpg']
+_mean_filename = '{}_mean.npy'.format(args.data_name)
+mean_file = os.path.join('data', args.data_name, _mean_filename)
+image_dir = os.path.join('data', args.data_name, 'test')
 
 pp = pprint.PrettyPrinter(indent=2)
 print "Using %s mode" % args.mode
@@ -40,7 +42,9 @@ else :
 
 def classify (net, image_file) :
   input_image = caffe.io.load_image(image_file)
-  prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
+  # predict takes any number of images, and formats them for the Caffe net
+  # automatically
+  prediction = net.predict([input_image])
   # print 'classifying image: ' + image_file
   # print 'prediction shape:', prediction[0].shape
   # plt.plot(prediction[0])
@@ -61,12 +65,13 @@ wrong = 0
 total_high = 0
 wrong_high = 0
 stats = {}
-ground_truth_extractor = re.compile("([0-9]+)-")
+ground_truth_extractor = re.compile("^([0-9]+)-")
 for image in sorted(os.listdir(image_dir)) :
   total += 1
-  (c, prob) = classify(net, image_dir + image)
+  (c, prob) = classify(net, os.path.join(image_dir, image))
   matched = ground_truth_extractor.match(image)
   ground_truth = int(matched.group(1))
+  print ground_truth, image
   if c != ground_truth :
     wrong += 1
     print "Wrong classification %d: %s with prob: %f"%(c, image, prob)
@@ -74,7 +79,7 @@ for image in sorted(os.listdir(image_dir)) :
   if prob < args.min_confidence :
     if c != ground_truth :
       wrong_high -= 1
-    c = -1 
+    c = -1
     total_high -= 1
   if ground_truth not in stats :
     stats[ground_truth] = {}
